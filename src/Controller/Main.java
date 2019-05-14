b8ab6007032ec9627113132c0426420deacec941
om088 <om088@neunzehn.informatik.uni-rostock.de>
2019-05-14 12:23:40 +0200

    Added package Controller with Main Class (in Main.java)

---
+++ src/Controller/Main.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import GUI.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new GUIMain();
+		GUIMain.main(args);
+		//System.out.println("test");
+		return;
+	}
+}
