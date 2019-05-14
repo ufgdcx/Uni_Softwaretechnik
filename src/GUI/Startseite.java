89c5d178089b8cd6f38f8eb761d79187a2c71f1a
om088 <om088@neunzehn.informatik.uni-rostock.de>
2019-05-14 12:57:37 +0200

    Changes to Startseite.java
    Added ActionListener in Startseite.java for testing.
    Removed unnecessary main method (previously used for testing).

--- src/GUI/Startseite.java
+++ src/GUI/Startseite.java
@@ -3,7 +3,7 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Startseite extends JFrame {
@@ -17,8 +17,8 @@ public class Startseite extends JFrame {
 	private JPasswordField pwd = new JPasswordField("Test");
 	private JLabel pwdLabel = new JLabel("Passwort: ");
 	private JLabel mailLabel = new JLabel("E-Mail-Adresse: ");
-	
-	
+	private Listener def = new Listener();
+
 	public Startseite(int width, int height) {
 		
 		panel.setLayout(new BorderLayout());
@@ -37,13 +37,26 @@ public class Startseite extends JFrame {
 		cpanel.add(pwdLabel);
 		cpanel.add(pwd);
 		
+		login.addActionListener(def);
+		registrieren.addActionListener(def);
+		
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Startseite");       
         this.getContentPane().add(panel);       
         this.setSize(width, height);
         this.setVisible(true);
 	}
-	public static void main(String[] args){
-		new Startseite(500,400);
+
+	private class Listener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==login){
+				System.out.println("login");
+			}
+			if(e.getSource()==registrieren){
+				System.out.println("registrieren");
+			}
+		}
 	}
+
 }
