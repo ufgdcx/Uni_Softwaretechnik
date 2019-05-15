902afd17e97d50f97cbc05ed5d43809606451c6a
Christoph <ufgdcx@gmail.com>
2019-05-15 14:07:41 +0200

    remove comments

--- src/Database/DBconn.java
+++ src/Database/DBconn.java
@@ -100,46 +100,6 @@ public class DatabaseCon {
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
-
-
-
-
-
-//        // Diese Eintraege werden zum
-//        // Verbindungsaufbau benoetigt.
-//        final String hostname = "localhost";
-//        final String port = "3306";
-//        final String dbname = "swt";
-//        final String user = "swt";
-//        final String password = "123";
-//
-//        Connection conn = null;
-//
-//        try {
-//            System.out.println("* Treiber laden");
-//            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-//        }
-//        catch (Exception e) {
-//            System.err.println("Unable to load driver.");
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            System.out.println("* Verbindung aufbauen");
-//            String url = "jdbc:mysql://"+hostname+":"+port+"/"+dbname;
-//            conn = DriverManager.getConnection(url, user, password);
-//
-//            System.out.println("* Datenbank-Verbindung beenden");
-//            conn.close();
-//        }
-//        catch (SQLException sqle) {
-//            System.out.println("SQLException: " + sqle.getMessage());
-//            System.out.println("SQLState: " + sqle.getSQLState());
-//            System.out.println("VendorError: " + sqle.getErrorCode());
-//            sqle.printStackTrace();
-//        }
-
-
     }
 }
 
