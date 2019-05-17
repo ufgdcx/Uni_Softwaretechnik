c69c591d416d24cb52efe98e42f0a7805e539e51
ufgdcx <ufgdcx@gmail.com>
2019-05-17 20:08:25 +0200

    revise Class Unterblock
    - revise or add constructor
    - add set methods
    - add get methods
    - add comments

--- src/Klassen/Unterblock.java
+++ src/Klassen/Unterblock.java
@@ -1,10 +1,27 @@
 package Klassen;
 
-public class Unterblock {
-
-	private String ubName = new String();
+public class Unterblock 
+{
+	// member variables
+	private String ubName;
 	private Float ubPunkte;
-
 	private Leistungsblock lBlock;
 	
+	// constructor
+	public Unterblock(String ubName, Float ubPunkte, Leistungsblock lBlock)
+	{
+		this.ubName = ubName;
+		this.ubPunkte = ubPunkte;
+		this.lBlock = lBlock;
+	}
+
+	// get methods
+	public String getUbName()	{return ubName;	}
+	public Float getUbPunkte()	{return ubPunkte;	}
+	public Leistungsblock getlBlock()	{return lBlock;	}
+
+	// set methods
+	public void setUbName(String ubName)	{this.ubName = ubName;	}
+	public void setUbPunkte(Float ubPunkte)	{this.ubPunkte = ubPunkte;	}
+	public void setlBlock(Leistungsblock lBlock)	{this.lBlock = lBlock;	}
 }
