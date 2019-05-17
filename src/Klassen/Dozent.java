d3db48b2cbb151d12bbdf970c1b3db2701d3effe
ufgdcx <ufgdcx@gmail.com>
2019-05-17 18:44:13 +0200

    revise Class Dozent - add set methods - add get methods - add comments - add add and remove methods

--- src/Klassen/Dozent.java
+++ src/Klassen/Dozent.java
@@ -1,26 +1,43 @@
 package Klassen;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Dozent extends Nutzer{
-
-    private String fakultaet = new String();
-    
-    private  ArrayList<Veranstaltung> veranstaltungen; //von Dozenten geleitete Veranstaltungen
+public class Dozent extends Nutzer
+{
+    // member variables
+    private String fakultaet;
+    private ArrayList<Veranstaltung> veranstaltungen; // von Dozenten geleitete Veranstaltungen
     private ArrayList<Gruppe> gruppen; //von Dozenten betreute Gruppen
 
-    public Dozent(String email,String passwort, String titel, String vorname, String name, String fakultaet){
-        super(email,passwort,titel,vorname,name);
+    public Dozent(String email,String passwort, String titel, String vorname, String name,
+                  String fakultaet)
+    {
+        super(email,passwort,titel,vorname,name); // make use of the superclass's constructor
         this.fakultaet = fakultaet;
     }
-    //getter und setter für Fakultaet
-    public String getFakultaet() {
 
-        return fakultaet;
-    }
+    // get methods
+    public String getFakultaet() { return fakultaet; }
+    public ArrayList<Veranstaltung> getVeranstaltungen(){ return this.veranstaltungen; }
+    public ArrayList<Gruppe> getGruppen(){ return this.gruppen; }
 
-    public void setFakultaet(String fakultaet) {
+    // set methods
+    public void setFakultaet(String fakultaet) { this.fakultaet = fakultaet; }
+    public void setVeranstaltungen(ArrayList<Veranstaltung> veranstaltungen) {this.veranstaltungen = veranstaltungen; }
+    public void setGruppen(ArrayList<Gruppe> gruppen){ this.gruppen = gruppen; }
 
-        this.fakultaet = fakultaet;
-    }
+    // add Veranstaltung
+    public void addVeranstaltung(Veranstaltung veranstaltung){ this.veranstaltungen.add(veranstaltung); }
+    // add Gruppe
+    public void addGruppe(Gruppe gruppe){ this.gruppen.add(gruppe); }
+
+    // remove Veranstaltung
+    public void removeVeranstaltung(int position){ this.veranstaltungen.remove(position); }
+    public void removeVeranstaltung(Veranstaltung veranstaltung){ this.veranstaltungen.remove(veranstaltung); }
+    // remove Gruppe
+    public void removeGruppe(int position){ this.gruppen.remove(position); }
+    public void removeGruppe(Gruppe gruppe){ this.gruppen.remove(gruppe); }
+
+
 }
