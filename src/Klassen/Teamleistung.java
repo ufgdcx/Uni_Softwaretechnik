fbaa2559b6291c54c1be9fdfd7c507cb875b0ceb
ufgdcx <ufgdcx@gmail.com>
2019-05-17 20:05:47 +0200

    revise Class Teamleistung
    - revise or add constructor
    - add set methods
    - add get methods
    - add comments

--- src/Klassen/Teamleistung.java
+++ src/Klassen/Teamleistung.java
@@ -2,9 +2,26 @@ package Klassen;
 
 public class Teamleistung {
 
-	private String teamleistungsblockname = new String();
+	// member variables
+	private String teamleistungsblockname;
 	private float tlPunkte;
-
 	private Team team;
 	
+	// constructor
+	public Teamleistung(String teamleistungsblockname, float tlPunkte, Team team)
+	{
+		this.teamleistungsblockname = teamleistungsblockname;
+		this.tlPunkte = tlPunkte;
+		this.team = team;
+	}
+
+	// get methods
+	public String getTeamleistungsblockname(){return teamleistungsblockname;}
+	public float getTlPunkte(){return tlPunkte;}
+	public Team getTeam(){return team;}
+
+	// set methods
+	public void setTeamleistungsblockname(String teamleistungsblockname){this.teamleistungsblockname = teamleistungsblockname;}
+	public void setTlPunkte(float tlPunkte){this.tlPunkte = tlPunkte;}
+	public void setTeam(Team team){this.team = team;}
 }
