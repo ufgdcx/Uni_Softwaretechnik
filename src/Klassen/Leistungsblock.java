2d6835f89c8f13568aa37ffe1fe02c4b6057ea2a
ufgdcx <ufgdcx@gmail.com>
2019-05-17 19:54:21 +0200

    revise Class Leistungsblock
    - revise or add constructor
    - add set methods
    - add get methods
    - add comments
    - add add and remove methods

--- src/Klassen/Leistungsblock.java
+++ src/Klassen/Leistungsblock.java
@@ -2,21 +2,36 @@ package Klassen;
 
 import java.util.ArrayList;
 
-public class Leistungsblock {
-
-    private String lbName = new String();
-
+public class Leistungsblock 
+{
+    // member variables
+    private String lbName;
     private Student student;
     private ArrayList<Unterblock> uBloecke; //Unterblöcke
     
-    //getter und setter für Klassen.Leistungsblock
-    public String getLbName() {
+    // constructor
 
-        return lbName;
-    }
-
-    public void setLbName(String lbName) {
-
+    public Leistungsblock(String lbName, Student student, ArrayList<Unterblock> uBloecke)
+    {
         this.lbName = lbName;
+        this.student = student;
+        this.uBloecke = uBloecke;
     }
+
+    // get methods
+    public String getLbName() { return lbName; }
+    public Student getStudent() { return student; }
+    public ArrayList<Unterblock> getuBloecke() { return uBloecke; }
+
+    // set methods
+    public void setLbName(String lbName) { this.lbName = lbName; }
+    public void setStudent(Student student) { this.student = student; }
+    public void setuBloecke(ArrayList<Unterblock> uBloecke) { this.uBloecke = uBloecke; }
+
+    // add Unterblock
+    public void addUnterblock(Unterblock unterblock){ this.uBloecke.add(unterblock); }
+
+    // remove Unterblock
+    public void removeUnterblock(int position){ this.uBloecke.remove(position); }
+    public void removeUnterblock(Unterblock unterblock){ this.uBloecke.remove(unterblock); }
 }
