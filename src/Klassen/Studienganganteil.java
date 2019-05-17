53d27d55f911dabd40374b93df36d42d8f89b892
ufgdcx <ufgdcx@gmail.com>
2019-05-17 20:02:54 +0200

    revise Class Studienganganteil
    - revise or add constructor
    - add set methods
    - add get methods
    - add comments

--- src/Klassen/Studienganganteil.java
+++ src/Klassen/Studienganganteil.java
@@ -1,10 +1,27 @@
 package Klassen;
 
-public class Studienganganteil {
-
-	private String Studiengang = new String();
+public class Studienganganteil 
+{
+	// member variables
+	private String Studiengang;
 	private int anteil;
-
 	private Team team;
 	
+	// constructor
+	public Studienganganteil(String studiengang, int anteil, Team team)
+	{
+		Studiengang = studiengang;
+		this.anteil = anteil;
+		this.team = team;
+	}
+
+	// get methods
+	public String getStudiengang(){return Studiengang;}
+	public int getAnteil(){return anteil;}
+	public Team getTeam(){return team;}
+
+	// set methods
+	public void setStudiengang(String studiengang){Studiengang = studiengang;}
+	public void setAnteil(int anteil){this.anteil = anteil;}
+	public void setTeam(Team team){this.team = team;}
 }
