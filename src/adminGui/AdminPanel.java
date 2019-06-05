521eb714c2b74fe4423bf0d56adc8e22961841c5
svenw11 <svbhagemann@web.de>
2019-06-05 18:04:21 +0200

    adminPanel now loads Veranstaltungen from Database and can delete them

--- src/adminGui/AdminPanel.java
+++ src/adminGui/AdminPanel.java
@@ -1,19 +1,24 @@
 package adminGui;
 
+import Database.DBrequest;
+import Database.DatabaseException;
+import Klassen.Veranstaltung;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
+import javax.swing.*;
 
 public class AdminPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
-	JList<String> veranstaltungen;
+
+	ArrayList<Veranstaltung> veranstaltungen = new ArrayList<>();
+	JList<String> veranstaltungslist;
+	DefaultListModel listModel = new DefaultListModel();
+	DBrequest dbr = DBrequest.getIntstance();
 	JButton deletebutton;
 	JButton editbutton;
 	JButton addbutton;
@@ -24,14 +29,20 @@ public class AdminPanel extends JPanel{
 		setPreferredSize(new Dimension(400, 400));
 		setSize(300,400);
 		//setBackground(Color.GREEN);
-		String[] v= {"SWT","ALGO","lange Veranstaltung zum testen wie das ganze angezeigt wird111111111111111111","d","d","d","d","d","d","d","d","d","a"};
-		veranstaltungen = new JList<String>(v);
-		veranstaltungen.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		veranstaltungen.setBounds(5, 5, 200, 200);
-		JScrollPane scrollPane = new JScrollPane(veranstaltungen);
+		try {
+			veranstaltungen = dbr.getAllVeranstaltungen();
+		}catch (DatabaseException de){
+			System.out.println(de.getErrorMsg());
+		}
+		veranstaltungslist = new JList<String>(listModel);
+		veranstaltungslist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		for(Veranstaltung v: veranstaltungen){
+			listModel.add(0,v.getName());
+		}
+		JScrollPane scrollPane = new JScrollPane(veranstaltungslist);
 		scrollPane.setBounds(20, 20, 362, 300);
 		add(scrollPane);
-		
+
 		deletebutton = new JButton("löschen");
 		deletebutton.setFocusable(false);
 		deletebutton.setBounds(20, 350, 100, 25);
@@ -62,6 +73,17 @@ public class AdminPanel extends JPanel{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource()==deletebutton) {
+				for(Veranstaltung v: veranstaltungen){
+					try {
+						if (v.getName().equals(veranstaltungslist.getSelectedValue()))
+							dbr.deleteVeranstaltung(v.getName());
+					}catch(DatabaseException dbe){
+						System.out.println(dbe.getErrorMsg());
+					}
+				}
+
+				listModel.remove(veranstaltungslist.getSelectedIndex());
+
 			}
 			if(e.getSource()==addbutton) {
 				CreateDialog cD = new CreateDialog();
