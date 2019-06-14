b57dc011f918ce27dd2c33d407e23bea83b74c5b
Oleg293 <oleg.mayer@web.de>
2019-06-14 17:15:15 +0200

    Changed DBrequest to Singelton pattern

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -19,7 +19,7 @@ import java.util.*;
 public class MainController {
 	protected GUIMain mainFrame;
 	private Nutzer n;
-	protected DBrequest dbr = new DBrequest();
+	protected DBrequest dbr = DBrequest.getIntstance();
 	
 	public MainController(GUIMain m) {
 		mainFrame = m;
