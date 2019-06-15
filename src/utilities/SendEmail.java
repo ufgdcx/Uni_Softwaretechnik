ec41f6b2ef6110ddcd8d4518a3f327006b0605e3
svenw11 <svbhagemann@web.de>
2019-06-15 15:00:48 +0200

    add javamail class to send mails add external library to use javamail

---
+++ src/utilities/SendEmail.java
@@ -0,0 +1,62 @@
+package utilities;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+
+    // Sender's email ID needs to be mentioned
+	private final static String from = "swt2019@web.de";
+	private final static String username = "swt2019";//change accordingly
+	private final static String password = "123456swt";//change accordingly
+    // Assuming you are sending email through relay.jangosmtp.net
+    private final static String host = "smtp.web.de";
+
+   public static void send(String to, String subject, String text) {
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "587");
+
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+	   }
+         });
+
+      try {
+	   // Create a default MimeMessage object.
+	   Message message = new MimeMessage(session);
+	
+	   // Set From: header field of the header.
+	   message.setFrom(new InternetAddress(from));
+	
+	   // Set To: header field of the header.
+	   message.setRecipients(Message.RecipientType.TO,
+               InternetAddress.parse(to));
+	
+	   // Set Subject: header field
+	   message.setSubject(subject);
+	
+	   // Now set the actual message
+	   message.setText(text);
+
+	   // Send message
+	   Transport.send(message);
+
+	   System.out.println("Sent message successfully....");
+
+      } catch (MessagingException e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
